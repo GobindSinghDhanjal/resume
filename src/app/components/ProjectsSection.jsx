c3da12027fbdefe38c2ba0af84a71933a3e59e1a
Gobind <gobindsinghdhanjal1@gmail.com>
2024-08-11 15:57:53 +0530

    fix some issues

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -25,40 +25,13 @@ const projectsData = [
   },
   {
     id: 3,
-    title: "E-commerce Application",
-    description: "Project 3 description",
+    title: "My Notes",
+    description: "Effortlessly manage notes with My Notes. Create, delete and edit your thoughts with ease.",
     image: "/images/projects/3.png",
     tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 4,
-    title: "Food Ordering Application",
-    description: "Project 4 description",
-    image: "/images/projects/4.png",
-    tag: ["All", "Mobile"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 5,
-    title: "React Firebase Template",
-    description: "Authentication and CRUD operations",
-    image: "/images/projects/5.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 6,
-    title: "Full-stack Roadmap",
-    description: "Project 5 description",
-    image: "/images/projects/6.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
+    gitUrl: "https://github.com/GobindSinghDhanjal/notes",
+    previewUrl: "https://notes-six-taupe.vercel.app/",
+  }
 ];
 
 const ProjectsSection = () => {
